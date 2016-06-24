ec64c9e6eb8d0c06364d3abec9d151d9786c6d8d
Lindsey <lindsey.font@gmail.com>
2016-06-24 15:03:48 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  namespace :api do
+    resources :bills
+  end
+
+  # ALWAYS KEEP AT THE VERY BOTTOM
+  get '*unmatched_route', to: 'home#index'
 end
--- webpack/routes.js
+++ webpack/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import NoMatch from './components/NoMatch';
 import Bills from './component/Bills';
@@ -7,6 +7,9 @@ import Bills from './component/Bills';
 export default (
   <Route>
     <Route path="/" component={App} />
+      <IndexRoute component={Bills} />
+      <Route path="/bills" component={Bills} />
+    </Route>
     <Route path="*" status={404} component={NoMatch}/>
   </Route>
 )
