ab2781721a2bbf409d3a16114e4bc5d9af01b2ab
Lindsey <lindsey.font@gmail.com>
2016-06-24 16:13:43 -0600

    Bill route

--- webpack/routes.js
+++ webpack/routes.js
@@ -3,12 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import NoMatch from './components/NoMatch';
 import Bills from './components/Bills';
+import Bill from './component/Bill';
 
 export default (
   <Route>
     <Route path="/" component={App} >
       <IndexRoute component={Bills} />
       <Route path="/bills" component={Bills} />
+      <Route path="/bills/:id" component={Bill} />
     </Route>
     <Route path="*" status={404} component={NoMatch}/>
   </Route>
